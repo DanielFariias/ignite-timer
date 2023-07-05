8d64d4062a6490a999d2687f26bff7a0c93703d9
DanielFariias <danielfariias15@gmail.com>
2023-07-05 11:31:36 -0300

    feat: :art: Created HeroSection

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,35 @@
+import { Play } from '@phosphor-icons/react'
+import * as S from './styles'
+
 export function Home() {
-  return <div>Home</div>
+  return (
+    <S.Container>
+      <form>
+        <S.FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input type="text" id="task" />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <input type="number" id="minutesAmount" />
+
+          <span>minutos.</span>
+        </S.FormContainer>
+
+        {/* CountDown */}
+        <S.Countdown>
+          <span>0</span>
+          <span>0</span>
+          <S.Separator>:</S.Separator>
+          <span>0</span>
+          <span>0</span>
+        </S.Countdown>
+
+        {/* button */}
+        <S.StartCountdownButton type="submit">
+          <Play size={24} />
+          Começar
+        </S.StartCountdownButton>
+      </form>
+    </S.Container>
+  )
 }
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,82 @@
+import { styled } from 'styled-components'
+
+export const Container = styled.main`
+  flex: 1;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 3.5rem;
+  }
+`
+
+export const FormContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  color: ${({ theme }) => theme['gray-100']};
+  font-size: 1.125rem;
+  font-weight: bold;
+  flex-wrap: wrap;
+`
+
+export const Countdown = styled.div`
+  font-family: 'Roboto Mono', monospace;
+  font-size: 10rem;
+  line-height: 8rem;
+  ${({ theme }) => theme['gray-100']};
+
+  display: flex;
+  gap: 1rem;
+
+  span {
+    background-color: ${({ theme }) => theme['gray-700']};
+    padding: 2rem 1rem;
+    border-radius: 8px;
+  }
+`
+
+export const Separator = styled.div`
+  padding: 2rem 0;
+
+  color: ${({ theme }) => theme['green-500']};
+
+  width: 4rem;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+`
+
+export const StartCountdownButton = styled.button`
+  width: 100%;
+  border: 0;
+  padding: 1rem;
+  border-radius: 8px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+
+  background-color: ${({ theme }) => theme['green-500']};
+  color: ${({ theme }) => theme['gray-100']};
+
+  transition: background-color 0.2s;
+
+  &:not(:disabled):hover {
+    background-color: ${({ theme }) => theme['green-700']};
+  }
+
+  &:disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+`
